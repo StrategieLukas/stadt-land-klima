e6f661bdc6e2d957fb782626e65f3a2226a1050c
Ondrej Brinkel <info@anzui.dev>
2023-09-20 00:01:13 +0200

    uses DIRECTUS_URL env var in directus client

--- src/frontend/plugins/directus.client.js
+++ src/frontend/plugins/directus.client.js
@@ -1,6 +1,6 @@
 import { createDirectus } from '@directus/sdk';
 import { rest, readItem, readItems } from '@directus/sdk/rest';
-const directusUrl = 'http://127.0.0.1:8081';
+const directusUrl = process.env.DIRECTUS_URL || 'http://127.0.0.1:8081';
 const directus = createDirectus(directusUrl).with(rest());
 
 export default defineNuxtPlugin(() => {
