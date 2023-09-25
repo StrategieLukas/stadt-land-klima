26908181e1494b1e5ae421338027f7af47b8bdb8
Ondrej Brinkel <info@anzui.dev>
2023-09-25 23:23:59 +0200

    adjusts link colors in drawer

--- src/frontend/components/TheDrawerSide.vue
+++ src/frontend/components/TheDrawerSide.vue
@@ -3,16 +3,16 @@
     <label for="page-drawer" class="drawer-overlay"></label>
     <ul tabindex="0" class="bg-light-green menu min-h-full w-80 p-4">
       <li>
-        <NuxtLink to="/" class="text-md btn btn-ghost normal-case text-base-100">{{ $t('home.nav_label') }}</NuxtLink>
+        <NuxtLink to="/" class="text-md btn btn-ghost normal-case text-black">{{ $t('home.nav_label') }}</NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/municipalities" class="text-md btn btn-ghost normal-case text-base-100">{{ $t('municipalities.nav_label') }}</NuxtLink>
+        <NuxtLink to="/municipalities" class="text-md btn btn-ghost normal-case text-black">{{ $t('municipalities.nav_label') }}</NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/measures" class="text-md btn btn-ghost normal-case text-base-100">{{ $t('measures.nav_label') }}</NuxtLink>
+        <NuxtLink to="/measures" class="text-md btn btn-ghost normal-case text-black">{{ $t('measures.nav_label') }}</NuxtLink>
       </li>
       <li v-for="page in pages" :key="page.id">
-        <NuxtLink :to="page.slug" class="text-md btn btn-ghost normal-case text-base-100">
+        <NuxtLink :to="page.slug" class="text-md btn btn-ghost normal-case text-black">
           {{ page.name }}
         </NuxtLink>
       </li>
--- src/frontend/components/TheFooter.vue
+++ src/frontend/components/TheFooter.vue
@@ -1,11 +1,11 @@
 <template lang="">
-  <footer class="bg-light-green px-2 py-4 text-base-100">
+  <footer class="bg-light-green px-2 py-4 text-black">
     <div class="mx-auto w-full max-w-screen-xl">
       <h3 class="text-md">{{ $t('sitemap') }}</h3>
 
       <ul class="grid-2-cols grid">
         <li v-for="page in pages" :key="page.id">
-          <NuxtLink :to="page.slug" class="text-md btn btn-link pl-0 normal-case text-base-100">
+          <NuxtLink :to="page.slug" class="text-md btn btn-link pl-0 normal-case text-black">
             {{ page.name }}
           </NuxtLink>
         </li>
