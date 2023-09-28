0d2e3bbceb1809d1a92cc68ebf3d5451a8c39058
TheodorWienert <theo@wienert-hamburg.de>
2023-09-28 18:22:29 +0200

    Small Fix

--- src/frontend/plugins/directus.client.js
+++ src/frontend/plugins/directus.client.js
@@ -4,15 +4,16 @@ import { staticToken } from "@directus/sdk/auth";
 import { rest, readItem, readItems, readTranslations } from "@directus/sdk/rest";
 import resolveFullLocaleCode from "~/shared/resolveFullLocaleCode.js";
 import createTranslator from "~/shared/createTranslator.js";
-const directusUrl = runtimeConfig.public.clientDirectusUrl || "http://127.0.0.1:8081"; //TODO
-let directus = createDirectus(directusUrl).with(rest());
+
 const locale = resolveFullLocaleCode(document.documentElement.getAttribute("lang"));
 
 export default defineNuxtPlugin(async () => {
 
+  
   const runtimeConfig = useRuntimeConfig();
+  const directusUrl = runtimeConfig.public.clientDirectusUrl || "http://127.0.0.1:8081"; 
   const token = runtimeConfig.public.directusToken;
-  directus = directus.with(staticToken(token));
+  const directus = createDirectus(directusUrl).with(rest()).with(staticToken(token));
   const translations = await directus.request(
     readTranslations({
       limit: -1,
