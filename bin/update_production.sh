f0b535803c5930e1e9ba1d728190b0e6627b0ac2
Ondrej Brinkel <info@anzui.dev>
2023-10-19 21:46:03 +0200

    adds command line options to update_production script

--- bin/update_production.sh
+++ bin/update_production.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+NO_DIRECTUS_BUILD=false
+NO_FRONTEND_BUILD=false
+NO_RESTART=false
+
+for arg in "$@"; do
+  case $arg in
+    --no-directus-build)
+      NO_DIRECTUS_BUILD=true
+      shift
+      ;;
+    --no-frontend-build)
+      NO_FRONTEND_BUILD=true
+      shift
+      ;;
+    --no-restart)
+      NO_RESTART=true
+      shift
+      ;;
+    --help)
+      echo "Options:
+--no-directus-build > prevents building directus
+--no-frontend-build > prevents building frontend
+--no-restart > prevents container restart"
+      exit
+      ;;
+    *)
+      ;;
+  esac
+done
+
 cd ..
 # no stashing to prevent changes beeing removed
 # git stash
@@ -8,8 +38,17 @@ git submodule sync --recursive
 git submodule update --recursive
 docker compose build && \
 docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up -d && \
-docker compose exec -T directus sh /build.sh
-docker compose exec -T frontend sh /build.sh
+if [[ $NO_DIRECTUS_BUILD != true ]]; then
+  echo "building directus ..."
+  docker compose exec -T directus sh /build.sh
+fi
+if [[ $NO_FRONTEND_BUILD != true ]]; then
+echo "building frontend ..."
+  docker compose exec -T frontend sh /build.sh
+fi
 cd bin
-./stop.sh
-./start_production.sh
+
+if [[ $NO_RESTART != true ]]; then
+  ./stop.sh
+  ./start_production.sh
+fi
