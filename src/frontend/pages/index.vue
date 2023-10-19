2eb8c7bf80f0ed9b7a001e6842c38ced18d7c323
Ondrej Brinkel <info@anzui.dev>
2023-10-19 21:13:55 +0200

    uses page with slug "index" on frontpage for contents

--- src/frontend/pages/index.vue
+++ src/frontend/pages/index.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
-    <h1>Hello world!</h1>
+    <article v-if="page" class="prose py-8" v-html="page.contents" />
+    <p v-else class="prose py-8">
+      {{ $t("page_not_found") }}
+    </p>
 
     <the-ranking :municipalities="municipalities" class=""></the-ranking>
   </div>
 </template>
 <script setup>
-const { $directus, $readItems } = useNuxtApp();
+const { $directus, $readItems, $t } = useNuxtApp();
+
+const { data: indexPages } = await useAsyncData("indexPages", () => {
+  return $directus.request(
+    $readItems("pages", {
+      filter: { slug: { _eq: 'index' } },
+      limit: 1,
+    }),
+  );
+});
+const page = indexPages.value[0] || null;
 
 //MetaTags
-const title = ref("Ranking");
+const title = page ? ref(page.name) : $t('page_not_found');
+
+//MetaTags
 useHead({
   title,
-});
-//
+});//
 
 const { data: municipalities } = await useAsyncData("municipalities", () => {
   return $directus.request(
