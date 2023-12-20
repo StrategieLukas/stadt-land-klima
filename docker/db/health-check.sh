670224a7d088a0199a50997df37c4563c1a09a67
Paul Severin <paul.severin@codecentric.de>
2023-12-20 09:08:11 +0100

    add comment

--- docker/db/health-check.sh
+++ docker/db/health-check.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# -h db sets the host to connect to to the hostname of this container (instead of localhost)
+# so that a non-loopback interface is used because only then the health check really indicates a full startup
+# see https://github.com/docker-library/postgres/issues/146#issuecomment-561557320
 pg_isready -h db -U $POSTGRES_USER
