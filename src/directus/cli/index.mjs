2b2bed19cbb7f60fe1efae534ab287b4e077013e
Paul Severin <paul.severin@codecentric.de>
2023-12-20 19:46:00 +0100

    add auth:set-token and auth:test tasks

--- src/directus/cli/index.mjs
+++ src/directus/cli/index.mjs
@@ -4,9 +4,10 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import exportTasks from './tasks/export/index.mjs';
 import importTasks from './tasks/import/index.mjs';
+import authTasks from "./tasks/authTasks.mjs";
 
 const _yargs = yargs(hideBin(process.argv));
 exportTasks(_yargs);
 importTasks(_yargs);
-
+authTasks(_yargs);
 _yargs.parse();
---
+++ src/directus/cli/tasks/authTasks.mjs
@@ -0,0 +1,70 @@
+import {authentication, createDirectus, readMe, rest, updateMe, staticToken} from '@directus/sdk';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import crypto from 'crypto';
+import createDirectusClient, {directusUrl} from "./shared/createDirectusClient.mjs";
+
+const envFile = '.env';
+
+export default function authTasks(yargs) {
+  return yargs
+    .command(
+      'auth:set-token',
+      'Generate a new static token and set it in Directus and .env',
+      async () => await setStaticToken()
+    )
+
+    .command(
+      'auth:test',
+      'Test if this tool can authenticate with Directus',
+      async () => await checkAuth()
+    );
+}
+
+async function checkAuth() {
+  try {
+    const directus = createDirectusClient();
+    await directus.request(readMe());
+    console.log('\x1b[32m%s\x1b[0m', 'Success!');
+
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+}
+
+async function setStaticToken() {
+  try {
+    dotenv.config({path: envFile});
+    const directus = createDirectus(directusUrl).with(authentication()).with(rest());
+
+    console.log("Logging in with admin credentials to get temporary access token...");
+    await directus.login(process.env.ADMIN_EMAIL, process.env.ADMIN_PASSWORD, {});
+
+    console.log("Generating static token...");
+    const newToken = crypto.randomBytes(24).toString('hex').slice(0, 32);
+
+    console.log("Setting static token in Directus...");
+    await directus.request(updateMe({token: newToken}));
+
+    console.log("Setting static token in .env ...");
+    let envContent = fs.readFileSync(envFile, 'utf8');
+    envContent = envContent.replace(/CLI_DIRECTUS_STATIC_TOKEN=.*/, `CLI_DIRECTUS_STATIC_TOKEN=${newToken}`);
+    fs.writeFileSync(envFile, envContent);
+
+    console.log("Testing static token...");
+    dotenv.config({path: envFile, override: true});
+
+    await createDirectus(directusUrl)
+      .with(staticToken(process.env.CLI_DIRECTUS_STATIC_TOKEN))
+      .with(rest())
+      .request(readMe());
+
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  console.log('\x1b[32m%s\x1b[0m', 'Success!');
+  process.exit(0);
+}
