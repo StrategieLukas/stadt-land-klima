c2d5bf080e5483b0d8ec7c72e4f967cc0b130760
Paul Severin <paul.severin@codecentric.de>
2024-01-07 15:12:31 +0100

    also import settings and remove project icon from exported settings

--- bin/import-all.sh
+++ bin/import-all.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-compose -f docker-compose.yaml -f docker-compose.prod.yaml exec directus
- /directus/cli/import-all.sh
+docker compose -f docker-compose.yaml -f docker-compose.prod.yaml exec directus /directus/cli/import-all.sh
