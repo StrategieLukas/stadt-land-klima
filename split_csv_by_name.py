b8e11a8c1b1bb86447bed9fe2a51153be77f845b
TheodorWienert <theo@wienert-hamburg.de>
2024-03-21 18:08:47 +0100

    Guide for Export and splitting script

---
+++ split_csv_by_name.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import sys
+
+if len(sys.argv) > 1:
+    inFile = sys.argv[1]
+else:
+    print("Please provide the input csv. In the execution of the script as an argument")
+    exit(1)
+
+
+
+def split_csv_by_column(csv_file, column_id):
+    df = pd.read_csv(csv_file)
+
+    groups = df.groupby(column_id)
+
+    output_folder = f"ratings_for_each_municipality"
+    os.makedirs(output_folder, exist_ok=True)
+
+    for group_name, group_df in groups:
+        file_name = os.path.join(output_folder, f"{group_name}_ratings.csv")
+        group_df.to_csv(file_name, index=False)
+        print(f"Saved '{group_name}' to '{file_name}'")
+
+csv_file = inFile
+column_id = "localteam_id.municipality_name"  
+split_csv_by_column(csv_file, column_id)
