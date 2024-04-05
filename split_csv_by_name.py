e5e07a8814da32ebf36975bb34ae4aa6b0629c12
LukasLanger <44872792+LukasLanger@users.noreply.github.com>
2024-04-05 12:07:23 +0200

    Update split_csv_by_name.py
    Added sector mapping, translated labels to German text and sorted everything in a sensible order

--- split_csv_by_name.py
+++ split_csv_by_name.py
@@ -12,16 +12,35 @@ else:
 
 def split_csv_by_column(csv_file, column_id):
     df = pd.read_csv(csv_file)
-    mapping = {
-        0: 'rot',
-        0.3333: 'gelb',
-        0.6666: 'hellgrün',
-        1: 'grün',
-        np.nan: 'nicht bewertet',
+    score_mapping = {
+        0: 'Rot',
+        0.3333: 'Gelb',
+        0.6666: 'Hellgrün',
+        1: 'Dunkelgrün',
+        np.nan: '-',
+    }
+    sector_mapping = {
+        "transport" : "Verkehr",
+        "energy" : "Energie",
+        "cpma" : "Klimaschutzmanagement",
+        "ann" : "Landwirtschaft, Natur & Ernährung",
+        "iec" : "Industrie, Wirtschaft & Konsum",
+        "bh" : "Gebäude & Wärme"
     }
 
-# Replace values according to the mapping
-    df['rating'] = df['rating'].replace(mapping)
+    # Replace values according to the mappings
+    df['rating'] = df['rating'].replace(score_mapping)
+    df['measure_id.sector'] = df['measure_id.sector'].replace(sector_mapping)
+
+    # Sort based on sector and then measure name (both alphabetically)
+    df = df.sort_values(['measure_id.sector', 'measure_id.name'])
+
+    # Change order of columns and rename to German labels
+    cols = ['measure_id.sector', 'measure_id.name', 'rating', 'date_updated', 'source', 'localteam_id.municipality_name']
+    df = df[cols]
+    df = df.rename(columns={'rating':'Bewertung', 'source':'Quelle', 'date_updated':'Zuletzt verändert', 'measure_id.name':'Name der Maßnahme', 'measure_id.sector':'Sektor', 'localteam_id.municipality_name':'Kommune'})
+
+
     groups = df.groupby(column_id)
 
     output_folder = f"ratings_for_each_municipality"
@@ -33,5 +52,5 @@ def split_csv_by_column(csv_file, column_id):
         print(f"Saved '{group_name}' to '{file_name}'")
 
 csv_file = inFile
-column_id = "localteam_id.municipality_name"
+column_id = "Kommune"
 split_csv_by_column(csv_file, column_id)
