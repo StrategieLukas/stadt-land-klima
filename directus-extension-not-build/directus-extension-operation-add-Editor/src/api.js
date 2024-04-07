8e80e213b72a18c202dc08b055d9d3fd8c58ce81
TheodorWienert <theo@wienert-hamburg.de>
2024-04-07 17:38:49 +0200

    Working Email

--- directus-extension-not-build/directus-extension-operation-add-Editor/src/api.js
+++ directus-extension-not-build/directus-extension-operation-add-Editor/src/api.js
@@ -7,14 +7,15 @@ export default {
 		accountability.admin = true;
 		logger.info(email, "email");
 		logger.info(localteam_id, "localteam_id");
-		
-		const { UsersService, RolesService, MailService, ItemsService} = services;
+
+		const { UsersService, RolesService, MailService, ItemsService } = services;
 		const schema = await getSchema();
-		const usersService = new UsersService({ schema,accountability});
+		const usersService = new UsersService({ schema, accountability });
 		const rolesService = new RolesService({ schema, accountability });
 		const localteamService = new ItemsService("localteams", {
 			schema,
-			accountability,});
+			accountability,
+		});
 		const mailService = new MailService({
 			schema: schema,
 			accountability: accountability,
@@ -38,38 +39,40 @@ export default {
 		const localteam = await localteamService.readOne(localteam_id, {
 			fields: ['id', 'municipality_name'],
 		});
+		const fullName = sender.first_name + " " + sender.last_name;
 		logger.info(sender);
 		logger.info(localteam);
 		// Create user first to verify uniqueness if unknown
 		if (isEmpty(user)) {
-			
+
 
 			//Create user
 			const resultUser = await usersService.createOne({ email, role: roles[0].id, status: 'invited', localteams: [{ localteam_id: localteam_id }] });
 
 			//send Email
 
-			const subjectLine = "You've been invited to Stadt.Land.Klima! as an " + roleName;
+			const subjectLine = "Sie wurden zu Stadt.Land.Klima eingeladen! als Redakteur*in";
 
 			await mailService.send({
-				to:  email,
+				to: email,
 				subject: subjectLine,
 				template: {
-					name: 'user-invitation',
+					name: 'email-template-invite',
 					data: {
 						url: url,
 						email: email,
-
+						fullName: fullName,
+						municipality_name: localteam.name
 					},
 				},
-			}); 
+			});
 
 		} else {
-			
-			if(user.role !== roles[0].id){
+
+			if (user.role !== roles[0].id) {
 				throw new Error("User has different role");
 			}
-				//Possibility to also use for update Maybe also for multiple loalteams per editor
+			//Possibility to also use for update Maybe also for multiple loalteams per editor
 		}
 
 	},
