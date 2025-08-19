e7ccd79bd9076cf53f9bc819c5f858df01072aca
StrategieLukas <44872792+StrategieLukas@users.noreply.github.com>
2025-08-19 19:33:25 +0100

    Use 100% colors instead of opacities, wip

--- src/frontend/shared/ratingColors.js
+++ src/frontend/shared/ratingColors.js
@@ -6,18 +6,18 @@ export const ratingColor = {
     null: "bg-rating-na",
   };
 
-export const ratingTextOpacity = {
-    3: "bg-opacity-10",
-    2: "bg-opacity-20",
-    1: "bg-opacity-20",
-    0: "bg-opacity-10",
-    null: "bg-opacity-10",
-  };
+// export const ratingTextOpacity = {
+//     3: "bg-opacity-10",
+//     2: "bg-opacity-20",
+//     1: "bg-opacity-20",
+//     0: "bg-opacity-10",
+//     null: "bg-opacity-10",
+//   };
 
-export const ratingHeaderOpacity = {
-    3: "bg-opacity-20",
-    2: "bg-opacity-30",
-    1: "bg-opacity-30",
-    0: "bg-opacity-20",
-    null: "bg-opacity-20"
-  };
+// export const ratingHeaderOpacity = {
+//     3: "bg-opacity-20",
+//     2: "bg-opacity-30",
+//     1: "bg-opacity-30",
+//     0: "bg-opacity-20",
+//     null: "bg-opacity-20"
+//   };
