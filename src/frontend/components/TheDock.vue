9d9e73bb789c69602a8cede130031d160d16b174
Jakob M. Krieger <jakobmkrieger@gmail.com>
2025-09-27 00:00:54 +0200

    small style changes

--- src/frontend/components/TheDock.vue
+++ src/frontend/components/TheDock.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="dock dock-lg bg-white/80 backdrop-blur-lg border-top border-gray-200 text-color-black flex justify-around items-center border-t-2">
+    <div class="dock dock-xl bg-white/80 backdrop-blur-lg border-top border-gray-200 text-color-black flex justify-around items-center border-t">
         <button 
             v-for="page in pages" 
             :key="page.id"
             :class="{ 'dock-active': isPageActive(page.slug) }"
             @click="followLink(page.slug)"
-            class="flex flex-col items-center min-w-0"
+            class="flex flex-col items-center min-w-0 min-h-[48px]"
         >
             <div class="size-[1.2em] [&>svg]:w-[24px] [&>svg]:h-[24px]" v-html="page.icon_svg"></div>
             <span class="dock-label text-center text-xs truncate max-w-full">{{ page.name }}</span>
@@ -15,9 +15,9 @@
             id="page-drawer-toggle"
             @click="toggleDrawer"
             :class="{ 'dock-active': isDrawerOpen }"
-            class="flex flex-col items-center min-w-0"
+            class="flex flex-col items-center min-w-0 min-h-[48px]"
         >
-            <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <svg class="size-[1.2em] w-[24px] h-[24px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
             </svg>
             <span class="dock-label text-center text-xs truncate max-w-full">{{ $t('generic.menu') }}</span>
