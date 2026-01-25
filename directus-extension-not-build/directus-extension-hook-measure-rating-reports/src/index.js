44876656b71d196108f8af6644d067d6fac37c03
rvn02 <rouven.meyer02@gmail.com>
2026-01-25 11:20:08 +0100

    updated query

--- directus-extension-not-build/directus-extension-hook-measure-rating-reports/src/index.js
+++ directus-extension-not-build/directus-extension-hook-measure-rating-reports/src/index.js
@@ -1,73 +1,85 @@
 import path from "path";
 import { writeFileSync, unlinkSync } from 'fs';
-
-function jsonToCsv(jsonString) {
-    // Parse the JSON string into an object
-    const jsonData = JSON.parse(jsonString);
-    
-    // Get the headers from the keys of the first object
-    const headers = Object.keys(jsonData[0]);
-
-    // Map each row in the JSON data to an array of values
-    const rows = jsonData.map(row => {
-        return headers.map(header => {
-            // Handling null values and ensuring quoting if necessary
-            const value = row[header] !== null ? row[header] : '';
-            return `"${value}"`; // Quoting values to handle commas in the data
-        }).join(',');
-    });
-
-    // Join the headers and rows into CSV format
-    return `${headers.join(',')}\n${rows.join('\n')}`;
-}
+import csvjson from "csvjson"
 
 
 export default ({ schedule }, { accountability, services, database, getSchema }) => {
 	const { MailService, ItemsService } = services
 
-    schedule('*/1 * * * *', async () => {
-		console.log("1 minute has passed");
+	// const period = '*/1 * * * *' // Every 1 minute
+	const period = '0 0 * * 0'	// At 00:00 on Sunday.
+    
+	schedule(period, async () => {
+		
+		// console.log("create and send rating measure report");
 
 		const schema = await getSchema();
 
-		const mailService = new MailService({
-			schema: schema,
-			accountability: accountability,
-		});
-
+		// Request data
 		const itemService_ratingMeasures = new ItemsService("ratings_measures", {
 			database,
 			schema: schema,
 		})
 
-		const ratingMeasures_results = await itemService_ratingMeasures.readByQuery({limit: -1})
+		const ratingMeasures_results = await itemService_ratingMeasures.readByQuery({
+			limit: -1,
+			fields: ["applicable", "approved", "choices", "current_progress", "date_created", "date_updated", 
+				"measure_published", "rating", "source", "status",
+				"id",
+				"localteam_id.municipality_id.name",
+				"measure_id.catalog_version.name",
+				"measure_id.catalog_version.id",
+				"measure_id.date_updated",
+				"measure_id.measure_id",
+				"measure_id.must_be_rated_again",
+				"measure_id.name"
+			]
+		})
 
+
+
+		// Create CSV File
 		const hookDir = path.join(process.cwd(), "/extensions/directus-extension-hook-measure-rating-reports");
 		
-        const timestamp = Date.now();
-        const fileName = `rating_measures-${timestamp}.csv`;
+		const d = new Date(); // Create a new Date object
+		const timestamp = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
+		const fileName = `rating_measures-${timestamp}.csv`;
         const filePath = path.join(hookDir, fileName);
 
-		const csv_string = jsonToCsv(JSON.stringify(ratingMeasures_results))
-        writeFileSync(filePath, csv_string, {flag: 'w'});
+		var options = {
+			delimiter   : ",",
+			wrap        : true
+		}
+
+		const csv_string = csvjson.toCSV(ratingMeasures_results, options)
+        
+		writeFileSync(filePath, csv_string, {flag: 'w'});
+
+
 
 		// Send Mail
-		const subjectLine =
-        "Stadt.Land.Klima Wochenbericht";
+		const mailService = new MailService({
+			schema: schema,
+			accountability: accountability,
+		});
 
-		const email = "TODO: info@stadt-land-klima.de";
+		const subjectLine = "Stadt.Land.Klima Wochenbericht";
+
+		const email = "massnahmen@stadt-land-klima.de";
 		await mailService.send({
 			to: email,
 			subject: subjectLine,
 			text: 'Please find attached the CSV file.',
 			attachments: [
 				{
-					filename: 'rating_measures.csv',
+					filename: fileName,
 					path: filePath
 				}
 			]
 		});
 
+
+		// Delete CSV File
 		unlinkSync(filePath)
 
     });
