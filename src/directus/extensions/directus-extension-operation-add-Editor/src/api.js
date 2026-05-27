d3e40d623b7c46c7f84b8d5a0f85a1a7161cb6f7
StrategieLukas <44872792+StrategieLukas@users.noreply.github.com>
2026-05-27 14:49:46 +0100

    Revert "Fix attempt"
    This reverts commit fffb03a6da5b66650ca3b621188ee50a08ab59b8.

--- src/directus/extensions/directus-extension-operation-add-Editor/src/api.js
+++ src/directus/extensions/directus-extension-operation-add-Editor/src/api.js
@@ -21,20 +21,9 @@ export default {
       schema: schema,
       accountability: accountability,
     });
-    
-    // Validate required parameters
-    if (!email) {
-      throw new Error("Email is required");
-    }
-    if (!localteam_id) {
-      throw new Error("Localteam ID is required");
-    }
-    
-    // Validate email format
     usersService.validateEmail(email);
     let url = usersService.inviteUrl(email);
-    
-    // Get the role
+    const user = await usersService.getUserByEmail(email);
     const roles = await rolesService.readByQuery({
       fields: ["*"],
       filter: {
@@ -43,52 +32,28 @@ export default {
         },
       },
     });
-    
-    if (isEmpty(roles)) {
-      throw new Error(`Role "${roleName}" not found`);
-    }
-    const roleId = roles[0].id;
-    
-    // Get sender info
-    if (!accountability.user) {
-      throw new Error("No authenticated user found in accountability context");
-    }
-    
+
     const sender = await usersService.readOne(accountability.user, {
       fields: ["id", "first_name", "last_name", "email"],
     });
-    
-    if (!sender) {
-      throw new Error(`Sender user with ID ${accountability.user} not found`);
-    }
-    
-    // Get localteam
     const localteam = await localteamService.readOne(localteam_id, {
       fields: ["id", "municipality_name"],
     });
-    
-    if (!localteam) {
-      throw new Error(`Localteam with ID ${localteam_id} not found`);
-    }
-    
     const fullName = sender.first_name + " " + sender.last_name;
     logger.info(`${fullName} hat eine neue Person (${email}) in das Lokalteam ${localteam.municipality_name} eingeladen!`);
-    
-    // Get existing user by email
-    const user = await usersService.getUserByEmail(email);
-    
     // Create user first to verify uniqueness if unknown
     if (isEmpty(user)) {
       //Create user
       const resultUser = await usersService.createOne({
         email,
-        role: roleId,
+        role: roles[0].id,
         status: "invited",
         localteams: [{ localteam_id: localteam_id }],
       });
 
       //send Email
-      const subjectLine = "Willkommen bei Stadt.Land.Klima!";
+      const subjectLine =
+        "Willkommen bei Stadt.Land.Klima!";
 
       try {
         await mailService.send({
@@ -101,23 +66,19 @@ export default {
               email: email,
               fullName: fullName,
               municipality_name: localteam.municipality_name,
-              projectName: "Stadt.Land.Klima",
-              projectUrl: "https://stadt-land-klima.de",
-              projectColor: "#1EA64A",
             },
           },
         });
       } catch (error) {
         if (process.env.SLK_ENV !== 'development') {
           logger.error(`Failed to send invitation email to ${email}`);
-          // Don't throw the error to allow user creation to succeed even if email fails
-          logger.warn(`User was created but email failed to send`);
+          throw error;
         } else {
           logger.warn(`Failed to send invitation email to ${email} (=this is expected behaviour in dev, where no e-mail server is set up.)`);
         }
       }
     } else {
-      if (user.role !== roleId) {
+      if (user.role !== roles[0].id) {
         throw new Error("User has different role");
       }
       //Possibility to also use for update Maybe also for multiple loalteams per editor
