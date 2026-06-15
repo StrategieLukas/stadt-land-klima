dbe60572ff659584d7bdf919f49a5872d573e242
StrategieLukas <44872792+StrategieLukas@users.noreply.github.com>
2026-06-15 13:01:18 +0100

    Add icon

--- src/frontend/components/DetailMunicipality.vue
+++ src/frontend/components/DetailMunicipality.vue
@@ -122,7 +122,7 @@
         :to="{ path: '/projects', query: { municipality: municipality.name } }"
         class="mt-4 flex items-center gap-3 rounded-sm bg-very-light-blue p-5 px-6 text-sm font-medium text-stats-dark shadow-list hover:bg-blue-100"
       >
-        <img src="~/assets/icons/icon_invest.svg" class="h-auto w-12 opacity-50 md:w-14 lg:w-18" />
+        <img src="~/assets/icons/icon_approved.svg" class="h-auto w-12 opacity-50 md:w-14 lg:w-18" />
         <h3 class="font-heading text-h2">
           {{ $t("projects.title") }}
           →
