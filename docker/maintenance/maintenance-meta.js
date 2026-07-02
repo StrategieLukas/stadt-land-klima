b136eed9e6a9aa47e3b7006c65a6d0356ebd41e6
Jakob M. Krieger <jakobmkrieger@gmail.com>
2026-07-02 15:01:03 +0200

    feat(maintenance): enhance maintenance mode with analytics integration and countdown refresh

--- docker/maintenance/maintenance-meta.js
+++ docker/maintenance/maintenance-meta.js
@@ -1,1 +1,4 @@
-window.__MAINTENANCE_STARTED_AT = "2026-07-02T12:30:26Z";
+window.__MAINTENANCE_STARTED_AT = "2026-07-02T12:53:52Z";
+window.__PLAUSIBLE_ANALYTICS_URL = "https://analytics.stadt-land-klima.de";
+window.__PLAUSIBLE_ANALYTICS_DOMAIN = "staging.stadt-land-klima.de";
+window.__PLAUSIBLE_UTM_CAMPAIGN = "maintenance_mode";
